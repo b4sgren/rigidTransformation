48a8981ffb4a66b791e3c1712c1cb629a702ace4
Brendon Forsgren <realpepino@gmail.com>
2020-06-01 20:18:07 -0600

    Changed test to not use eigen while I experimented

--- tests/so2_tests.cpp
+++ tests/so2_tests.cpp
@@ -1,15 +1,19 @@
 #include <gtest/gtest.h>
 #include <Eigen/Core>
+#include <iostream>
 
-// #include "so2.h"
+#include "so2.h"
 
 TEST(AskForTheRotationMatrix, ReturnsTheRotationMatrix)
 {
-    Eigen::Matrix2d R_true;
-    R_true << 1, 0, 0, 1;
+    // Eigen::Matrix2d R_true;
+    // R_true << 1, 0, 0, 1;
+
+    double v{3.3};
+    SO2<double> v2(v);
 
     // SO2<double> R(R_true);
     
     // EXPECT_TRUE(R_true.isApprox(R.R()));
-    EXPECT_TRUE(true);
+    EXPECT_TRUE(v == v2.R());
 }
