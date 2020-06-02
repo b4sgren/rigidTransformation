ae4fa07fafd19dcd186cc2fa7e9991a8f3cf678c
Brendon Forsgren <realpepino@gmail.com>
2020-06-01 19:27:38 -0600

    Added a constructor and simple function to transformation base. Going to do this incrementally.

--- include/rigidTransformation/transformation_base.h
+++ include/rigidTransformation/transformation_base.h
@@ -3,9 +3,18 @@
 
 #include <Eigen/Core>
 
+//This class is just a base defining some functions that all inherited classes will contain
+
 template <class T>
 class TransformationBase
 {
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+public:
+    TransformationBase(const Eigen::MatrixBase<T> &mat);
+    virtual Eigen::MatrixBase<T> R() const;    
+
+private:
+    Eigen::MatrixBase<T> _arr;
 
 };
 
