03bdda7df2cf10ecb47a719acaee50589aec3323
Brendon Forsgren <realpepino@gmail.com>
2020-06-01 20:14:41 -0600

    Basic template working with an int

--- include/rigidTransformation/so2.h
+++ include/rigidTransformation/so2.h
@@ -4,13 +4,17 @@
 #include "transformation_base.h"
 #include <Eigen/Core>
 
-template<class T> //Replace all Matrix<T,2,2> with a using statement
+
+template<typename T> //Replace all Matrix<T,2,2> with a using statement
 class SO2 : public TransformationBase<T>
 {
+    using TransformationBase<T>::val;
 public:
-    SO2(Eigen::Matrix<T, 2, 2> mat) : _arr{mat} {}
-
-    Eigen::Matrix<T,2,2> R() const { return _arr; }
+    // SO2(Eigen::Matrix<T, 2, 2> mat) : _arr{mat} {}
+    // Eigen::Matrix<T,2,2> R() const { return _arr; }
+    SO2(T v): TransformationBase<T>{v} {}
+    // SO2(T v) { val = v; }
+    // T R() const { return val; }
 
 private:
 };
