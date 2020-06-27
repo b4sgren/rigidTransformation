a3b4304006a1b17e4f5ad8e734d5a6e8b9dca845
Brendon Forsgren <realpepino@gmail.com>
2020-06-27 10:39:33 -0600

    Added se2 class

---
+++ include/rigidTransformation/se2.h
@@ -0,0 +1,16 @@
+#ifndef SE2_H
+#define SE2_H
+
+#include <Eigen/Dense>
+#include <random>
+#include <iostream>
+#include <cmath>
+
+constexpr double PI = 3.141592653589793238462643383279;
+
+template<typename T>
+class SE2
+{
+};
+
+#endif
---
+++ tests/se2_tests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <eigen3/unsupported/Eigen/MatrixFunctions>
+#include <random>
+
+#include "so3.h"
+
+TEST(temp, temp_)
+{
+    EXPECT_TRUE(true);
+}
