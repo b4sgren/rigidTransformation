f88fa7f775bf83de719002abf1904ec5b9969b6c
Brendon Forsgren <realpepino@gmail.com>
2020-07-28 20:26:22 -0600

    Added the SE3 files

---
+++ include/rigidTransformation/se3.h
@@ -0,0 +1,26 @@
+#ifndef SE3_H
+#define SE3_H
+
+#include <random>
+#include <iostream>
+#include <cmath>
+
+#include <eigen3/Eigen/Dense>
+
+#include "utils.h"
+
+template<typename F>
+class SE3
+{
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    using Mat4F = Eigen::Matrix<F,4,4>;
+    using Mat3F = Eigen::Matrix<F,3,3>;
+    using Vec3F = Eigen::Matrix<F,3,1>;
+    using Vec6F = Eigen::Matrix<F,6,1>;
+public:
+
+private:
+    Mat4F _arr;
+};
+
+#endif
---
+++ tests/se3_tests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <eigen3/unsupported/Eigen/MatrixFunctions>
+#include <random>
+
+#include "se3.h"
+
+TEST(Blah, Blah)
+{
+    EXPECT_TRUE(true);
+}
