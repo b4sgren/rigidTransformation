ee6fa000b3689906b7d162ec1471761d366379ea
Brendon Forsgren <realpepino@gmail.com>
2021-02-22 20:41:18 -0700

    Started rewriting the SE2 class

--- include/rigidTransformation/se2.h
+++ include/rigidTransformation/se2.h
@@ -8,6 +8,9 @@
 
 #include "utils.h"
 
+namespace rigidTransform
+{
+
 template<typename F>
 class SE2
 {
@@ -16,243 +19,18 @@ class SE2
     using Vec2F = Eigen::Matrix<F,2,1>;
     using Vec3F = Eigen::Matrix<F,3,1>;
     using Map3F = Eigen::Map<Mat3F>;
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 public:
-    //Add a constructor that takes in an Eigen::Affine something and taking a F*
-    //To accept a pointer _arr might have to be an Eigen::Map...
-    SE2() = default;
-    SE2(const Mat3F &mat): _arr{mat} {}
-    SE2(const Mat2F &R, const Vec2F &t)
-    {
-        _arr = Mat3F::Zero();
-        _arr.template block<2,2>(0,0) = R;
-        _arr.template block<2,1>(0,2) = t;
-        _arr(2,2) = F(1.0);
-    }
-    SE2(F* mat): _arr{Map3F(mat)} {}
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    SE2() : arr_(data_) { arr_.setIdentity(); }
 
-    SE2 operator*(const SE2 &rhs)
-    {
-        return SE2(this->T() * rhs.T());
-    }
-
-    Vec3F operator*(const Vec3F &v)
-    {
-        return this->T() * v;
-    }
-
-    Vec2F operator*(const Vec2F &v)
-    {
-        Vec3F v1;
-        v1 << v(0), v(1), F(1.0);
-        Vec3F vp{(*this) * v1};
-        return vp.template segment<2>(0);
-    }
-
-    bool operator==(const SE2 &rhs)
-    {
-        return this->T().isApprox(rhs.T());
-    }
-
-    Mat3F T() const { return _arr; }
-    Mat2F R() const { return _arr.template block<2,2>(0,0); }
-    Vec2F t() const {return _arr.template block<2,1>(0,2); }
-    F* data() {return _arr.data(); }
-    const F* data() const {return _arr.data(); }
-    
-    Mat3F Adj() const 
-    {
-        Mat2F J;
-        J << 0.0, 1.0, -1.0, 0.0;
-
-        Mat3F adj{Mat3F::Identity()};
-        adj.template block<2,1>(1,0) = J * this->t();
-        adj.template block<2,2>(1,1) = this->R();
-        return adj;
-    }
-
-    SE2 inv() const
-    {
-        Mat2F R_inv{this->R().transpose()};
-        Vec2F t_inv{-R_inv * this->t()};
-        return SE2(R_inv, t_inv);
-    }
-
-    void selfInv()
-    {
-        _arr.template block<2,2>(0,0) = this->R().transpose();
-        _arr.template block<2,1>(0,2) = -this->R() * this->t();
-    }
-
-    Vec3F transa(const Vec3F &v)
-    {
-        return (*this) * v;
-    }
-
-    Vec3F transp(const Vec3F &v)
-    {
-        return (*this).inv() * v;
-    }
-
-    Vec2F transa(const Vec2F &v)
-    {
-        return (*this) * v;
-    }
-
-    Vec2F transp(const Vec2F &v)
-    {
-        return (*this).inv() * v;
-    }
-
-    SE2 boxplus(const Vec3F &d)
-    {
-        return (*this) * Exp(d);
-    }
-
-    Vec3F boxminus(const SE2 &T)
-    {
-        return Log(T.inv() * (*this));
-    }
-
-    bool isValidTransformation() const 
-    {
-        F det{this->R().determinant()};
-        bool valid_rot{abs(det - F(1.0)) < 1e-8};
-        bool homogeneous{this->T()(2,2) == F(1.0)};
-
-        return (valid_rot && homogeneous);
-    }
-
-    static SE2 random()
-    {
-        static std::random_device rd;
-        static std::mt19937 generator(rd());
-        static std::uniform_real_distribution<F> dist(F(-PI), F(PI)); //Could edit to get bigger range for translation
-
-        F ang{dist(generator)};
-        Vec2F t;
-        t << dist(generator), dist(generator);
-
-        F ct{cos(ang)}, st{sin(ang)};
-        Mat2F arr;
-        arr << ct, -st, st, ct;
-        return SE2(arr, t);
-    }
-
-    static SE2 fromAngleAndVec(const F ang, const Vec2F &t)
-    {
-        F ct{cos(ang)}, st{sin(ang)};
-        Mat2F R;
-        R << ct, -st, st, ct;
-        return SE2(R, t);
-    }
-
-    static Mat3F hat(const Vec3F &w)
-    {
-        Mat3F log_T{Mat3F::Zero()};
-        log_T.template block<2,2>(0,0) = skew2(w(0));
-        log_T.template block<2,1>(0,2) = w.template segment<2>(1);
-        return log_T;
-    }
-
-    static Vec3F vee(const Mat3F &log_T)
-    {
-        Vec3F w;
-        w << log_T(1,0), log_T(0,2), log_T(1,2);
-        return w;
-    }
-
-    Mat3F log() const 
-    {
-        return SE2::log(this->T());
-    }
-
-    static Mat3F log(const SE2 &T)
-    {
-        return T.log();
-    }
-
-    static Mat3F log(const Mat3F &T)
-    {
-        F theta{atan2(T(1,0), T(0,0))};
-        Vec2F t{T.template block<2,1>(0,2)};
-
-        F A, B;
-        if(abs(theta) > 1e-6)
-        {
-            A = sin(theta) / theta;
-            B = (1 - cos(theta)) / theta;
-        }
-        else 
-        {
-            A = 1.0 - pow(theta,2) / 6.0 + pow(theta, 4) / 120.0;
-            B = theta / 2.0 - pow(theta, 3) / 24.0 + pow(theta,5) / 720.0;
-        }
-
-        F normalizer{1.0 / (pow(A,2) + pow(B,2))};
-        Mat2F temp;
-        temp << A, B, -B, A;
-        Mat2F V_inv{normalizer * temp};
-
-        Mat3F log_T{Mat3F::Zero()};
-        log_T.template block<2,1>(0,2) = V_inv * t;
-        log_T(0,1) = -theta;
-        log_T(1,0) = theta;
-
-        return log_T;
-    }
-
-    Vec3F Log() const
-    {
-        return SE2::Log(this->T());
-    }
-
-    static Vec3F Log(const SE2 &T)
-    {
-        return T.Log();
-    }
-
-    static Vec3F Log(const Mat3F &T)
-    {
-        return SE2::vee(SE2::log(T));
-    }
-
-    static SE2 exp(const Mat3F &log_T)
-    {
-        F theta{log_T(1,0)};
-
-        F A, B;
-        if(abs(theta) > 1e-6)
-        {
-            A = sin(theta) / theta;
-            B = (1 - cos(theta)) / theta;
-        }
-        else 
-        {
-            A = 1.0 - pow(theta,2) / 6.0 + pow(theta, 4) / 120.0;
-            B = theta / 2.0 - pow(theta, 3) / 24.0 + pow(theta,5) / 720.0;
-        }
-
-        Mat2F V;
-        V << A, -B, B, A;
-        Vec2F t{V * log_T.template block<2,1>(0,2)};
-
-        return SE2::fromAngleAndVec(theta, t);
-    }
-
-    static SE2 Exp(const Vec3F &w)
-    {
-        Mat3F log_T{SE2::hat(w)};
-        return SE2::exp(log_T);
-    }
-
-    static SE2 Identity()
-    {
-        return SE2{Mat3F::Identity()};
-    }
+    Mat3F T() const { return arr_; }
 
 private:
-    Mat3F _arr;
+    F data_[9];
+public:
+    Eigen::Map<Mat3F> arr_;
 };
+
+} // namespace rigidTransform
 
 #endif
---
+++ tests/test_se2.cpp
@@ -0,0 +1,393 @@
+#include <gtest/gtest.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <eigen3/unsupported/Eigen/MatrixFunctions>
+#include <random>
+#include <vector>
+
+#include "se2.h"
+
+namespace rt = rigidTransform;
+using SE2d = rt::SE2<double>;
+
+double getRandomDouble(double min, double max)
+{
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_real_distribution<double> dist(min, max);
+
+    return dist(generator);
+}
+
+Eigen::Vector2d getRandomVector(double min, double max)
+{
+    Eigen::Vector2d v;
+    v << getRandomDouble(min, max), getRandomDouble(min, max);
+    return v;
+}
+
+bool compareMat(const Eigen::Ref<const Eigen::Matrix3d> &T1, const Eigen::Ref<const Eigen::Matrix3d> &T2)
+{
+    return T1.isApprox(T2);
+}
+
+class SE2_Fixture : public ::testing::Test
+{
+public:
+    SE2_Fixture()
+    {
+    }
+
+    ~SE2_Fixture(){}
+protected:
+    std::vector<SE2d> transforms_;
+};
+
+TEST_F(SE2_Fixture, DefaultInitialization)
+{
+    SE2d T;
+    Eigen::Matrix3d I(Eigen::Matrix3d::Identity());
+    EXPECT_TRUE(compareMat(I, T.T()));
+}
+
+// TEST(SE2_Element, AskedForMatrix_ReturnsHomogeneousTransformationMatrix)
+// {
+//     Eigen::Matrix3d T;
+//     T << 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0;
+//     Eigen::Matrix2d R{T.block<2,2>(0,0)};
+//     Eigen::Vector2d t{T.block<2,1>(0,2)};
+
+//     SE2<double> T1{T};
+//     EXPECT_TRUE(T.isApprox(T1.T()));
+//     EXPECT_TRUE(R.isApprox(T1.R()));
+//     EXPECT_TRUE(t.isApprox(T1.t()));
+// }
+
+// TEST(GenerateRandomSE2Element, IfTransformationIsValid_ReturnsTrue)
+// {
+//     for(int i{0}; i!=100; ++i)
+//     {
+//         SE2<double> T{SE2<double>::random()};
+//         EXPECT_TRUE(T.isValidTransformation());
+//     }
+// }
+
+// TEST(FromAngleAndVec, AngleAndTranslationVector_ReturnTransformationMatrix)
+// {
+//     for(int i{0}; i!=100; ++i)
+//     {
+//         double angle{getRandomDouble(-PI, PI)};
+//         Eigen::Vector2d t{getRandomVector(-10.0, 10.0)};
+
+//         SE2<double> T{SE2<double>::fromAngleAndVec(angle, t)};
+//         double ct{cos(angle)}, st{sin(angle)};
+//         Eigen::Matrix3d T_true;
+//         T_true << ct, -st, t(0), st, ct, t(1), 0.0, 0.0, 1.0;
+
+//         EXPECT_TRUE(T_true.isApprox(T.T()));
+//     }
+// }
+
+// TEST(FromPointer, PointerInColumnMajorOrder_ReturnsTransformationMatrix)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         SE2<double> T{SE2<double>::random()};
+//         SE2<double> T2{T.data()};
+
+//         EXPECT_TRUE(T == T2);
+//     }
+// }
+
+// TEST(GroupMultiplication, TwoSE2Objects_ReturnConcatenatedSE2Object)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         SE2<double> T1{SE2<double>::random()}, T2{SE2<double>::random()};
+
+//         SE2<double> T3{T1 * T2};
+//         Eigen::Matrix3d T3_true;
+//         T3_true.block<2,2>(0,0) = T1.R() * T2.R();
+//         T3_true.block<2,1>(0,2) = T1.t() + T1.R() * T2.t();
+//         T3_true(2,2) = 1.0;
+
+//         EXPECT_TRUE(T3_true.isApprox(T3.T()));
+//     }
+// }
+
+// TEST(Inverse, SE2Object_ReturnInverse)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         SE2<double> T{SE2<double>::random()};
+//         SE2<double> T_inv{T.inv()};
+
+//         SE2<double> res{T * T_inv};
+
+//         EXPECT_TRUE(Eigen::Matrix3d::Identity().isApprox(res.T()));
+//     }
+// }
+
+// TEST(InverseInPlace, SE2Object_InvertsSelf)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         SE2<double> T{SE2<double>::random()};
+//         Eigen::Matrix3d T_mat{T.T()};
+
+//         T.selfInv();
+//         Eigen::Matrix3d res{T.T() * T_mat};
+
+//         EXPECT_TRUE(Eigen::Matrix3d::Identity().isApprox(res));
+//     }
+// }
+
+// TEST(ActiveTransformation, SE2ObjectAndPoint_ReturnsTransformedPoint)
+// {
+//     for(int i{0}; i !=100; ++i)
+//     {
+//         SE2<double> T{SE2<double>::random()};
+//         Eigen::Vector2d v{getRandomVector(-10.0, 10.0)};
+//         Eigen::Vector3d vn; // HOmogeneous coordinates
+//         vn << v(0), v(1), 1.0;
+
+//         /*
+//         Note that you get out what you pass in. If you want a vector in
+//         homogeneous coordinates then pass in a vector in homogeneous coordinates
+//         */
+//         Eigen::Vector2d vp{T.transa(v)};
+//         Eigen::Vector3d vn_p{T.transa(vn)};
+
+//         Eigen::Vector2d vp_true{T.R() * v + T.t()};
+
+//         EXPECT_TRUE(vp_true.isApprox(vp));
+//         EXPECT_TRUE(vp_true.isApprox(vn_p.segment<2>(0)));
+//     }
+// }
+
+// TEST(PassiveTransformation, SE2ObjectAndPoint_ReturnsTransformedPoint)
+// {
+//     for(int i{0}; i !=100; ++i)
+//     {
+//         SE2<double> T{SE2<double>::random()};
+//         Eigen::Vector2d v{getRandomVector(-10.0, 10.0)};
+//         Eigen::Vector3d vn; // Homogeneous coordinates
+//         vn << v(0), v(1), 1.0;
+
+//         /*
+//         Note that you get out what you pass in. If you want a vector in
+//         homogeneous coordinates then pass in a vector in homogeneous coordinates
+//         */
+//         Eigen::Vector2d vp{T.transp(v)};
+//         Eigen::Vector3d vn_p{T.transp(vn)};
+
+//         Eigen::Vector2d vp_true{T.inv().R() * v + T.inv().t()};
+
+//         EXPECT_TRUE(vp_true.isApprox(vp));
+//         EXPECT_TRUE(vp_true.isApprox(vn_p.segment<2>(0)));
+//     }
+// }
+
+// TEST(HatOperator, ThreeVector_3by3MatrixOfLieAlgebra)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         Eigen::Vector2d t{getRandomVector(-10.0, 10.0)};
+//         double ang{getRandomDouble(-PI, PI)};
+//         Eigen::Vector3d w;
+//         w << ang, t(0), t(1);
+
+//         Eigen::Matrix3d log_T{SE2<double>::hat(w)};
+//         Eigen::Matrix3d log_T_true;
+//         log_T_true << 0, -ang, t(0), ang, 0, t(1), 0, 0, 0;
+
+//         EXPECT_TRUE(log_T_true.isApprox(log_T));
+//     }
+// }
+
+// TEST(VeeOperator, se2Element_Return3Vector)
+// {
+//     for(int i{0}; i !=100; ++i)
+//     {
+//         Eigen::Vector2d t{getRandomVector(-10.0, 10.0)};
+//         double ang{getRandomDouble(-PI, PI)};
+//         Eigen::Vector3d w_true;
+//         w_true << ang, t(0), t(1);
+
+//         Eigen::Matrix3d log_T{SE2<double>::hat(w_true)};
+//         Eigen::Vector3d w{SE2<double>::vee(log_T)};
+
+//         EXPECT_TRUE(w_true.isApprox(w));
+//     }
+// }
+
+// TEST(MatrixLogarithm, SE2Element_ReturnElementOfLieAlgebra)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         SE2<double> T{SE2<double>::random()};
+
+//         Eigen::Matrix3d log_T{T.log()};
+//         Eigen::Matrix3d log_T2{SE2<double>::log(T)};
+//         Eigen::Matrix3d log_T3{SE2<double>::log(T.T())};
+//         Eigen::Matrix3d log_T_true{T.T().log()};
+
+//         EXPECT_TRUE(log_T_true.isApprox(log_T));
+//         EXPECT_TRUE(log_T_true.isApprox(log_T2));
+//         EXPECT_TRUE(log_T_true.isApprox(log_T3));
+//     }
+// }
+
+// TEST(TaylorMatrixLogarithm, SE2Element_ReturnsElementOfLieAlgebra)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         double ang{getRandomDouble(-1e-6, 1e-6)};
+//         Eigen::Vector2d t{getRandomVector(-10.0, 10.0)};
+
+//         SE2<double> T{SE2<double>::fromAngleAndVec(ang, t)};
+
+//         Eigen::Matrix3d log_T{T.log()};
+//         Eigen::Matrix3d log_T_true{T.T().log()};
+
+//         EXPECT_TRUE(log_T_true.isApprox(log_T));
+//     }
+// }
+
+// TEST(MatrixLogarith, SE2Element_Returns3Vector)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         SE2<double> T{SE2<double>::random()};
+
+//         Eigen::Vector3d log_T{T.Log()};
+//         Eigen::Vector3d log_T2{SE2<double>::Log(T)};
+//         Eigen::Vector3d log_T3{SE2<double>::Log(T.T())};
+
+//         Eigen::Matrix3d temp{T.T().log()};
+//         Eigen::Vector3d log_T_true{SE2<double>::vee(temp)};
+
+//         EXPECT_TRUE(log_T_true.isApprox(log_T));
+//         EXPECT_TRUE(log_T_true.isApprox(log_T2));
+//         EXPECT_TRUE(log_T_true.isApprox(log_T3));
+//     }
+// }
+
+// TEST(MatrixExponential, se2Element_ReturnSE2Element)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         double ang{getRandomDouble(-PI, PI)};
+//         Eigen::Vector2d t{getRandomVector(-10.0, 10.0)};
+//         Eigen::Vector3d w;
+//         w << ang, t(0), t(1);
+
+//         Eigen::Matrix3d log_T{SE2<double>::hat(w)};
+
+//         SE2<double> T{SE2<double>::exp(log_T)};
+//         Eigen::Matrix3d T_true{log_T.exp()};
+
+//         EXPECT_TRUE(T_true.isApprox(T.T()));
+//     }
+// }
+
+// TEST(TaylorMatrixExponential, se2Element_ReturnsSE2Element)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         double ang{getRandomDouble(-1e-6, 1e-6)};
+//         Eigen::Vector2d t{getRandomVector(-10.0, 10.0)};
+//         Eigen::Vector3d w;
+//         w << ang, t(0), t(1);
+
+//         Eigen::Matrix3d log_T{SE2<double>::hat(w)};
+
+//         SE2<double> T{SE2<double>::exp(log_T)};
+//         Eigen::Matrix3d T_true{log_T.exp()};
+
+//         EXPECT_TRUE(T_true.isApprox(T.T()));
+
+//     }
+// }
+
+// TEST(MatrixExponential, 3Vector_ReturnsSE2Element)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         double ang{getRandomDouble(-PI, PI)};
+//         Eigen::Vector2d t{getRandomVector(-10.0, 10.0)};
+//         Eigen::Vector3d w;
+//         w << ang, t(0), t(1);
+
+//         SE2<double> T{SE2<double>::Exp(w)};
+
+//         Eigen::Matrix3d log_T{SE2<double>::hat(w)};
+//         Eigen::Matrix3d T_true{log_T.exp()};
+
+//         EXPECT_TRUE(T_true.isApprox(T.T()));
+//     }
+// }
+
+// TEST(Adjoint, SE2ElementAnd3Vector_ReturnsCorrectComposition)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         SE2<double> T{SE2<double>::random()};
+
+//         double ang{getRandomDouble(-PI, PI)};
+//         Eigen::Vector2d t{getRandomVector(-10.0, 10.0)};
+//         Eigen::Vector3d delta;
+//         delta << ang, t(0), t(1);
+
+//         SE2<double> res1{T * SE2<double>::Exp(delta)};
+//         Eigen::Vector3d temp{T.Adj() * delta};
+//         SE2<double> temp2{SE2<double>::Exp(temp)};
+//         SE2<double> res2{temp2 * T};
+
+//         if(!(res1 == res2))
+//         {
+//             int debug = 1;
+//             SE2<double> blah{temp2 * T};
+//         }
+
+//         EXPECT_TRUE(res1 == res2);
+//     }
+// }
+
+// TEST(BoxPlus, SE2And3Vector_ReturnsNewSE2Element)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         SE2<double> T{SE2<double>::random()};
+//         Eigen::Vector2d t{getRandomVector(-10.0, 10.0)};
+//         double ang{getRandomDouble(-PI, PI)};
+//         Eigen::Vector3d d;
+//         d << t, ang;
+
+//         SE2<double> res{T.boxplus(d)};
+//         SE2<double> res_true{T * SE2<double>::Exp(d)};
+
+//         EXPECT_TRUE(res == res_true);
+//     }
+// }
+
+// TEST(BoxMinus, 2SE2Elements_ReturnsDifference)
+// {
+//     for(int i{0}; i != 100; ++i)
+//     {
+//         SE2<double> T1{SE2<double>::random()}, T2{SE2<double>::random()};
+
+//         Eigen::Vector3d d{T1.boxminus(T2)};
+//         SE2<double> T{T2.boxplus(d)};
+
+//         EXPECT_TRUE(T == T1);
+//     }
+// }
+
+// TEST(Identity, AskedForIdentity_ReturnsIdentity)
+// {
+//     Eigen::Matrix3d I{Eigen::Matrix3d::Identity()};
+//     SE2<double> T{SE2<double>::Identity()};
+
+//     EXPECT_TRUE(I.isApprox(T.T()));
+// }
