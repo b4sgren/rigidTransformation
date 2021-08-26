1297d3f63f119b089fe3c2810bb6729e3ef7a068
Brendon Forsgren <realpepino@gmail.com>
2021-08-26 17:09:06 -0600

    restyled se3 class

--- include/rigidTransformation/se3.h
+++ include/rigidTransformation/se3.h
@@ -1,154 +1,138 @@
-#ifndef SE3_H
-#define SE3_H
+#ifndef RIGIDTRANSFORMATION_SE3_H_
+#define RIGIDTRANSFORMATION_SE3_H_
+
+#include <eigen3/Eigen/Dense>
+#include <cmath>
 
 #include <random>
 #include <iostream>
-#include <cmath>
-
-#include <eigen3/Eigen/Dense>
 
 #include "utils.h"
 #include "quaternion.h"
 
-namespace rigidTransform
-{
+namespace rigidTransform {
 
 template<typename F>
-class SE3
-{
-    using Mat3F = Eigen::Matrix<F,3,3>;
-    using Vec7F = Eigen::Matrix<F,7,1>;
-    using Vec6F = Eigen::Matrix<F,6,1>;
-    using Vec4F = Eigen::Matrix<F,4,1>;
-    using Vec3F = Eigen::Matrix<F,3,1>;
-public:
+class SE3 {
+    using Mat3F = Eigen::Matrix<F, 3, 3>;
+    using Vec7F = Eigen::Matrix<F, 7, 1>;
+    using Vec6F = Eigen::Matrix<F, 6, 1>;
+    using Vec4F = Eigen::Matrix<F, 4, 1>;
+    using Vec3F = Eigen::Matrix<F, 3, 1>;
+
+ public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-    SE3() : arr_(data_), q_(data_+3)
-    {
+    SE3() : arr_(data_), q_(data_+3) {
         arr_.setZero();
         arr_(3) = 1.0;
     }
 
-    SE3(const F* data) : arr_(const_cast<F*>(data)), q_(const_cast<F*>(data+3)) {}
+    explicit SE3(const F* data) : arr_(const_cast<F*>(data)),
+                                  q_(const_cast<F*>(data+3)) {}
 
-    SE3(const Eigen::Ref<const Vec7F> &T) : arr_(data_), q_(data_+3)
-    {
+    explicit SE3(const Eigen::Ref<const Vec7F> &T) : arr_(data_), q_(data_+3) {
         arr_ = T;
     }
 
-    SE3(const F& r, const F& p, const F& y, const Eigen::Ref<const Vec3F> &t) : arr_(data_), q_(data_+3)
-    {
+    SE3(const F& r, const F& p, const F& y,
+        const Eigen::Ref<const Vec3F> &t) : arr_(data_), q_(data_+3) {
         arr_.template head<3>() = t;
-        arr_.template tail<4>() = Quaternion<F>(r,p,y).q();
+        arr_.template tail<4>() = Quaternion<F>(r, p, y).q();
     }
 
-    SE3(const Eigen::Ref<const Mat3F> &R, const Eigen::Ref<const Vec3F> &t): arr_(data_), q_(data_+3)
-    {
+    SE3(const Eigen::Ref<const Mat3F> &R,
+        const Eigen::Ref<const Vec3F> &t): arr_(data_), q_(data_+3) {
         arr_.template head<3>() = t;
         arr_.template tail<4>() = Quaternion<F>::fromR(R).q();
     }
 
-    SE3(const Quaternion<F> &q, const Eigen::Ref<const Vec3F> &t) : arr_(data_), q_(data_+3)
-    {
+    SE3(const Quaternion<F> &q,
+        const Eigen::Ref<const Vec3F> &t) : arr_(data_), q_(data_+3) {
         arr_.template head<3>() = t;
         arr_.template tail<4>() = q.q();
     }
 
-    SE3(const SE3 &T): arr_(data_), q_(data_+3)
-    {
+    SE3(const SE3 &T): arr_(data_), q_(data_+3) {
         arr_ = T.T();
     }
 
-    SE3& operator=(const SE3& rhs)
-    {
+    SE3& operator=(const SE3& rhs) {
         arr_ = rhs.T();
         return (*this);
     }
 
-    SE3 operator*(const SE3& T2)
-    {
+    SE3 operator*(const SE3& T2) {
         Quaternion<F> q(q_ * T2.q_);
         Vec3F trans(t() + q_.rota(T2.t()));
-        return SE3(q,trans);
+        return SE3(q, trans);
     }
 
     Vec7F T() const { return arr_; }
     Mat3F R() const { return q_.R(); }
     Vec4F q() const { return q_.q(); }
-    Quaternion<F> quat() const { return q_; } // better name for this
+    Quaternion<F> quat() const { return q_; }
     Vec3F t() const { return arr_.template head<3>(); }
     F x() const { return arr_(0); }
     F y() const { return arr_(1); }
     F z() const { return arr_(2); }
     Vec3F euler() const { return q_.euler(); }
 
-    SE3 inverse() const
-    {
+    SE3 inverse() const {
         Quaternion<F> q_inv = q_.inverse();
         Vec3F t_inv = -q_inv.rota(t());
         return SE3(q_inv, t_inv);
     }
 
-    void inverse_()
-    {
+    void inverse_() {
         q_.inverse_();
         arr_.template head<3>() = -q_.rota(t());
     }
 
-    Vec3F transa(const Eigen::Ref<const Vec3F> &v)
-    {
+    Vec3F transa(const Eigen::Ref<const Vec3F> &v) {
         return t() + q_.rota(v);
     }
 
-    Vec3F transp(const Eigen::Ref<const Vec3F> &v)
-    {
+    Vec3F transp(const Eigen::Ref<const Vec3F> &v) {
         return inverse().transa(v);
     }
 
-    SE3 boxplusr(const Eigen::Ref<const Vec6F> &tau)
-    {
+    SE3 boxplusr(const Eigen::Ref<const Vec6F> &tau) {
         return (*this) * SE3::Exp(tau);
     }
 
-    Vec6F boxminusr(const SE3 &T)
-    {
+    Vec6F boxminusr(const SE3 &T) {
         return SE3::Log(T.inverse() * (*this));
     }
 
-    SE3 boxplusl(const Eigen::Ref<const Vec6F> &tau)
-    {
+    SE3 boxplusl(const Eigen::Ref<const Vec6F> &tau) {
         return SE3::Exp(tau) * (*this);
     }
 
-    Vec6F boxminusl(const SE3 &T)
-    {
+    Vec6F boxminusl(const SE3 &T) {
         return SE3::Log((*this) * T.inverse());
     }
 
-    Eigen::Matrix<F,6,6> Adj() const
-    {
+    Eigen::Matrix<F, 6, 6> Adj() const {
         Mat3F rot = R().transpose();
-        Eigen::Matrix<F,6,6> adj;
+        Eigen::Matrix<F, 6, 6> adj;
         adj.setZero();
-        adj.template block<3,3>(0,0) = rot;
-        adj.template block<3,3>(3,3) = rot;
-        adj.template block<3,3>(0,3) = skew3<F>(t()) * rot;
+        adj.template block<3, 3>(0, 0) = rot;
+        adj.template block<3, 3>(3, 3) = rot;
+        adj.template block<3, 3>(0, 3) = skew3<F>(t()) * rot;
         return adj;
     }
 
-    F* data()
-    {
+    F* data() {
         return arr_.data();
     }
 
-    static SE3 fromAxisAngleAndt(const Eigen::Ref<const Vec3F> &v, const Eigen::Ref<const Vec3F> &t)
-    {
+    static SE3 fromAxisAngleAndt(const Eigen::Ref<const Vec3F> &v,
+                                 const Eigen::Ref<const Vec3F> &t) {
         Quaternion<F> q(Quaternion<F>::fromAxisAngle(v));
         return SE3(q, t);
     }
 
-    static SE3 random()
-    {
+    static SE3 random() {
         static std::random_device rd;
         static std::mt19937 gen(rd());
         static std::uniform_real_distribution<F> dist(-10, 10);
@@ -160,13 +144,11 @@ public:
         return SE3(q, t);
     }
 
-    static SE3 Identity()
-    {
+    static SE3 Identity() {
         return SE3();
     }
 
-    static SE3 Exp(const Eigen::Ref<const Vec6F> &tau)
-    {
+    static SE3 Exp(const Eigen::Ref<const Vec6F> &tau) {
         auto rho(tau.template head<3>());
         auto theta(tau.template tail<3>());
         F norm(theta.norm());
@@ -174,57 +156,55 @@ public:
         Quaternion<double> q(Quaternion<double>::Exp(theta));
 
         Vec3F t;
-        if(abs(norm) > 1e-8)
-        {
+        if (abs(norm) > 1e-8) {
             Mat3F thetax(skew3<F>(theta));
-            Mat3F V = Mat3F::Identity() + (1 - cos(norm))/(norm*norm)*thetax + (norm - sin(norm))/pow(norm,3) * thetax*thetax;
+            Mat3F V = Mat3F::Identity() + (1 - cos(norm))/(norm*norm)*thetax +
+                (norm - sin(norm))/pow(norm, 3) * thetax*thetax;
             t = V * rho;
-        }
-        else
+        } else {
             t = rho;
+        }
 
         return SE3(q, t);
     }
 
-    Vec6F Log() const
-    {
+    Vec6F Log() const {
         return SE3::Log(*this);
     }
 
-    static Vec6F Log(const SE3 &T)
-    {
+    static Vec6F Log(const SE3 &T) {
         Vec3F logq(Quaternion<F>::Log(T.q_));
         F theta(logq.norm());
 
         Vec3F rho;
-        if(abs(theta) > 1e-8)
-        {
+        if (abs(theta) > 1e-8) {
             F A(sin(theta)/theta), B((1-cos(theta))/(theta*theta));
             Mat3F thetax(skew3<F>(logq));
-            Mat3F V_inv(Mat3F::Identity() - 0.5*thetax + (1-A/(2*B))/(theta*theta)*thetax*thetax);
+            Mat3F V_inv(Mat3F::Identity() - 0.5*thetax +
+                (1-A/(2*B))/(theta*theta)*thetax*thetax);
             rho = V_inv * T.t();
-        }
-        else
+        } else {
             rho = T.t();
+        }
 
         Vec6F logT;
         logT << rho, logq;
         return logT;
     }
 
-private:
+ private:
     F data_[7];
-public:
+
+ public:
     Eigen::Map<Vec7F> arr_;
     Quaternion<F> q_;
 };
 
 template<typename F>
-std::ostream& operator <<(std::ostream &os, const SE3<F> &T)
-{
+std::ostream& operator <<(std::ostream &os, const SE3<F> &T) {
     os << T.T().transpose();
     return os;
 }
-} // namespace rigidTransform
+}  // namespace rigidTransform
 
-#endif
+#endif  // RIGIDTRANSFORMATION_SE3_H_
