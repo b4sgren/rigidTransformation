f3a0c775d7baf6a55a7d78247a258da877f3d0a4
Brendon Forsgren <realpepino@gmail.com>
2024-10-16 21:08:09 -0500

    Updated the SO3 example

--- examples/so3_example.cpp
+++ examples/so3_example.cpp
@@ -2,62 +2,80 @@
 This is an example using the SO2 class. This class will find the mean of a
 bunch of rotation matrices in the plane
 */
-#include "rigidTransformation/so3.h"
-#include "rigidTransformation/utils.h"
-#include <eigen3/Eigen/Dense>
-
+#include <ceres/autodiff_manifold.h>
 #include <ceres/ceres.h>
+
+#include <eigen3/Eigen/Dense>
+#include <iostream>
 #include <random>
 #include <vector>
-#include <iostream>
+
+#include "rigidTransformation/so3.h"
+#include "rigidTransformation/utils.h"
 
 namespace rt = rigidTransform;
 
 class RotationResidual {
-
- public:
-    RotationResidual(const rt::SO3<double> &R, const Eigen::Matrix3d &cov) :
-                                                            R_{R} {
+   public:
+    RotationResidual(const rt::SO3<double>& R, const Eigen::Matrix3d& cov) : R_{R} {
         info_ = cov.inverse().llt().matrixU();
     }
 
     template <typename T>
-    bool operator() (const T* const r, T* residuals) const {
+    bool operator()(const T* const r, T* residuals) const {
         rt::SO3<T> R(r);
         Eigen::Map<Eigen::Matrix<T, 3, 1>> res(residuals);
         res = info_ * rt::SO3<T>::Log(R.inverse() * R_);
         return true;
     }
 
- private:
+    static ceres::CostFunction* Create(rt::SO3<double>& R, const Eigen::Matrix3d& cov) {
+        return new ceres::AutoDiffCostFunction<RotationResidual, 3, 9>(new RotationResidual(R, cov));
+    }
+
+   private:
     rt::SO3<double> R_;
     Eigen::Matrix3d info_;
 };
-using RotationCost = ceres::AutoDiffCostFunction<RotationResidual, 3, 9>;
 
-class SO3_Parameterization {
- public:
+class SO3Manifold {
+   public:
     template <typename T>
-    bool operator() (const T* rot, const T* delta, T* R_plus_delta) const {
-        rt::SO3<T> R(rot), Rpd(R_plus_delta);
-        Eigen::Map<const Eigen::Matrix<T, 3, 1>> t(delta);
-        Rpd = R.template boxplusr<T>(t);
+    bool Plus(const T* rot, const T* delta, T* R_plus_delta) const {
+        rt::SO3<T> R(rot), res(R_plus_delta);
+        Eigen::Map<const Eigen::Matrix<T, 3, 1>> theta(delta);
+        res = R.template boxplusr<T>(theta);
 
         return true;
     }
+
+    template <typename T>
+    bool Minus(const T* rot1, const T* rot2, T* diff) const {
+        rt::SO3<T> R1(rot1), R2(rot2);
+        Eigen::Map<Eigen::Matrix<T, 3, 1>> theta(diff);
+        theta = R1.boxminusr(R2);
+
+        return true;
+    }
+
+    static ceres::Manifold* Create() {
+        return new ceres::AutoDiffManifold<SO3Manifold, 9, 3>;
+    }
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     // Establish the mean rotation
     rt::SO3<double> R = rt::SO3<double>::random();
 
     // Setup noise distribution
-    double theta_std{3 * rt::PI/180};
+    double theta_std{3 * rt::PI / 180};
     Eigen::Matrix3d cov =
         Eigen::Vector3d{theta_std, theta_std, theta_std}.asDiagonal();
     std::random_device rd;
     std::mt19937 engine(rd());
     std::normal_distribution<> generator(0, theta_std);
+
+    ceres::Manifold* so3_manifold = SO3Manifold::Create();
 
     // Generate Noisy Measurements
     Eigen::Vector3d theta;
@@ -68,23 +86,19 @@ int main(int argc, char *argv[]) {
         measurements.emplace_back(R.boxplusr<double>(theta));
     }
     rt::SO3<double> R_hat = measurements[0];
-    std::cout << "Initial Guess:\n" << R_hat << std::endl;
-
+    std::cout << "Initial Guess:\n"
+              << R_hat << std::endl;
 
     // Setup ceres problem
     ceres::Problem problem;
 
     // Add the reisduals
     for (rt::SO3<double> m : measurements) {
-        ceres::CostFunction *cost = new RotationCost(new
-                RotationResidual(m, cov));
+        ceres::CostFunction* cost = RotationResidual::Create(m, cov);
         problem.AddResidualBlock(cost, nullptr, R_hat.data());
     }
-
-    // Set the local parameterization
-    ceres::LocalParameterization *rot_param = new
-            ceres::AutoDiffLocalParameterization<SO3_Parameterization, 9, 3>();
-    problem.SetParameterization(R_hat.data(), rot_param);
+    // Set the manifold
+    problem.SetManifold(R_hat.data(), so3_manifold);
 
     // Set solver options
     ceres::Solver::Options options;
@@ -94,8 +108,10 @@ int main(int argc, char *argv[]) {
     ceres::Solver::Summary summary;
     ceres::Solve(options, &problem, &summary);
 
-    std::cout << "True Mean:\n" << R << std::endl;
-    std::cout << "Est Mean:\n" << R_hat << std::endl;
+    std::cout << "True Mean:\n"
+              << R << std::endl;
+    std::cout << "Est Mean:\n"
+              << R_hat << std::endl;
     std::cout << summary.FullReport() << std::endl;
 
     return 0;
