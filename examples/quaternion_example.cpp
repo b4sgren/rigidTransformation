3a870c56e5d2fce28a69fc988b07e9f0829481b3
Brendon Forsgren <realpepino@gmail.com>
2024-10-16 21:20:25 -0500

    Updated the quaternion example

--- examples/quaternion_example.cpp
+++ examples/quaternion_example.cpp
@@ -2,62 +2,81 @@
 This is an example using the SO2 class. This class will find the mean of a
 bunch of rotation matrices in the plane
 */
-#include "rigidTransformation/quaternion.h"
-#include "rigidTransformation/utils.h"
-#include <eigen3/Eigen/Dense>
-
+#include <ceres/autodiff_manifold.h>
 #include <ceres/ceres.h>
+
+#include <eigen3/Eigen/Dense>
+#include <iostream>
 #include <random>
 #include <vector>
-#include <iostream>
+
+#include "rigidTransformation/quaternion.h"
+#include "rigidTransformation/utils.h"
 
 namespace rt = rigidTransform;
 
 class RotationResidual {
-
- public:
-    RotationResidual(const rt::Quaternion<double> &q,
-                     const Eigen::Matrix3d &cov) : q_{q} {
+   public:
+    RotationResidual(const rt::Quaternion<double>& q,
+                     const Eigen::Matrix3d& cov) : q_{q} {
         info_ = cov.inverse().llt().matrixU();
     }
 
     template <typename T>
-    bool operator() (const T* const r, T* residuals) const {
+    bool operator()(const T* const r, T* residuals) const {
         rt::Quaternion<T> q(r);
         Eigen::Map<Eigen::Matrix<T, 3, 1>> res(residuals);
         res = info_ * rt::Quaternion<T>::Log(q.inverse() * q_);
         return true;
     }
 
- private:
+    static ceres::CostFunction* Create(rt::Quaternion<double>& q, Eigen::Matrix3d& cov) {
+        return new ceres::AutoDiffCostFunction<RotationResidual, 3, 4>(new RotationResidual(q, cov));
+    }
+
+   private:
     rt::Quaternion<double> q_;
     Eigen::Matrix3d info_;
 };
-using RotationCost = ceres::AutoDiffCostFunction<RotationResidual, 3, 4>;
 
-class Quat_Parameterization {
- public:
+class QuatManifold {
+   public:
     template <typename T>
-    bool operator() (const T* rot, const T* delta, T* q_plus_delta) const {
-        rt::Quaternion<T> q(rot), qpd(q_plus_delta);
-        Eigen::Map<const Eigen::Matrix<T, 3, 1>> t(delta);
-        qpd = q.template boxplusr<T>(t);
+    bool Plus(const T* rot, const T* delta, T* q_plus_delta) const {
+        rt::Quaternion<T> q(rot), res(q_plus_delta);
+        Eigen::Map<const Eigen::Matrix<T, 3, 1>> theta(delta);
+        res = q.template boxplusr<T>(theta);
 
         return true;
     }
+
+    template <typename T>
+    bool Minus(const T* rot1, const T* rot2, T* diff) const {
+        rt::Quaternion<T> q1(rot1), q2(rot2);
+        Eigen::Map<Eigen::Matrix<T, 3, 1>> dq(diff);
+        dq = q1.boxminusr(q2);
+
+        return true;
+    }
+
+    static ceres::Manifold* Create() {
+        return new ceres::AutoDiffManifold<QuatManifold, 4, 3>;
+    }
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     // Establish the mean rotation
     rt::Quaternion<double> q = rt::Quaternion<double>::random();
 
     // Setup noise distribution
-    double theta_std{3 * rt::PI/180};
+    double theta_std{3 * rt::PI / 180};
     Eigen::Matrix3d cov =
         Eigen::Vector3d{theta_std, theta_std, theta_std}.asDiagonal();
     std::random_device rd;
     std::mt19937 engine(rd());
     std::normal_distribution<> generator(0, theta_std);
+
+    ceres::Manifold* quat_manifold = QuatManifold::Create();
 
     // Generate Noisy Measurements
     Eigen::Vector3d theta;
@@ -68,23 +87,19 @@ int main(int argc, char *argv[]) {
         measurements.emplace_back(q.boxplusr<double>(theta));
     }
     rt::Quaternion<double> q_hat = measurements[0];
-    std::cout << "Initial Guess:\n" << q_hat << std::endl;
-
+    std::cout << "Initial Guess:\n"
+              << q_hat << std::endl;
 
     // Setup ceres problem
     ceres::Problem problem;
 
     // Add the reisduals
     for (rt::Quaternion<double> m : measurements) {
-        ceres::CostFunction *cost = new RotationCost(new
-                RotationResidual(m, cov));
+        ceres::CostFunction* cost = RotationResidual::Create(m, cov);
         problem.AddResidualBlock(cost, nullptr, q_hat.data());
     }
-
-    // Set the local parameterization
-    ceres::LocalParameterization *rot_param = new
-            ceres::AutoDiffLocalParameterization<Quat_Parameterization, 4, 3>();
-    problem.SetParameterization(q_hat.data(), rot_param);
+    // Set the manifold
+    problem.SetManifold(q_hat.data(), quat_manifold);
 
     // Set solver options
     ceres::Solver::Options options;
@@ -94,8 +109,10 @@ int main(int argc, char *argv[]) {
     ceres::Solver::Summary summary;
     ceres::Solve(options, &problem, &summary);
 
-    std::cout << "True Mean:\n" << q << std::endl;
-    std::cout << "Est Mean:\n" << q_hat << std::endl;
+    std::cout << "True Mean:\n"
+              << q << std::endl;
+    std::cout << "Est Mean:\n"
+              << q_hat << std::endl;
     std::cout << summary.FullReport() << std::endl;
 
     return 0;
